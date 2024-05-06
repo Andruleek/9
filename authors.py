9499b6c92ba6acc870ed827b712e98c7df83ba8c
ExtymAndriy <150171957+ExtymAndriy@users.noreply.github.com>
2024-05-06 21:57:54 +0300

    yes

--- authors.py
+++ authors.py
@@ -2,37 +2,48 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# Функція для отримання даних про авторів
-def scrape_authors():
-    authors = {}
-    page_num = 1
+# Функція для отримання посилань на всі сторінки з цитатами
+def get_all_pages():
+    base_url = "http://quotes.toscrape.com"
+    pages = [base_url]
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
     while True:
-        url = f'http://quotes.toscrape.com/page/{page_num}/'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        quotes = soup.find_all('div', class_='quote')
-        if not quotes:  # Якщо немає цитат, завершуємо цикл
+        next_page = soup.find("li", class_="next")
+        if next_page:
+            next_page_link = next_page.find("a")['href']
+            next_page_url = base_url + next_page_link
+            pages.append(next_page_url)
+            response = requests.get(next_page_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+        else:
             break
+    return pages
+
+# Функція для отримання інформації про авторів цитат
+def get_authors(pages):
+    authors_info = {}
+    for page in pages:
+        response = requests.get(page)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        quotes = soup.find_all("div", class_="quote")
         for quote in quotes:
-            author_name = quote.find('small', class_='author').text
-            author_element = quote.find('span', class_='author')
-            if author_element:  # Перевіряємо, чи є елемент автора
-                author_url = author_element.find_next_sibling('a')
-                if author_url:  # Перевіряємо, чи є посилання на автора
-                    author_url = author_url['href']
-                else:
-                    author_url = None
-                if author_name not in authors:
-                    authors[author_name] = {'author_name': author_name, 'author_url': author_url}
-        page_num += 1
+            author_name = quote.find("small", class_="author").text
+            author_url = quote.find("a")['href']
+            if author_name not in authors_info:
+                authors_info[author_name] = author_url
+    return authors_info
 
-    return list(authors.values())
+# Функція для збереження інформації про авторів у файл JSON
+def save_authors_to_json(authors_info):
+    with open('authors.json', 'w') as f:
+        json.dump(authors_info, f, indent=4)
 
-# Отримання даних про авторів
-authors_data = scrape_authors()
+# Отримуємо посилання на всі сторінки з цитатами
+pages = get_all_pages()
 
-# Запис даних у файл authors.json
-with open('authors.json', 'w') as file:
-    json.dump(authors_data, file, indent=4)
+# Отримуємо інформацію про авторів
+authors_info = get_authors(pages)
 
-print("Дані про авторів успішно записано у файл authors.json")
+# Зберігаємо інформацію про авторів у файл JSON
+save_authors_to_json(authors_info)
